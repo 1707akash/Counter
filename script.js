dbe46237694302ae9ebafa6becbc1a4dd2aa2e8e
1707akash <1707akash@gmail.com>
2024-01-15 22:38:51 +0530

    js edited

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let add = document.querySelector(".add");
 let sub = document.querySelector(".sub");
 let reset = document.querySelector(".reset");
 let reading = parseInt(counter.innerText);
+let lastVal= document.querySelector("h2");
 // console.log(counter);
 
 add.addEventListener("click",()=>{
@@ -18,6 +19,7 @@ sub.addEventListener("click",()=>{
 });
 
 reset.addEventListener("click",()=>{
+    lastVal.innerText= `Last Value: ${reading}`;
     reading=0;
     counter.innerText= reading;
 });
